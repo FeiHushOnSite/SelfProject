95f298bcf4d4392f922416f4e534f696064cd98d
feiyuji <ksjisl@126.com>
2019-07-03 14:44:46 +0800

    update

--- normaltest/src/leedcode/mergeArray.java
+++ normaltest/src/leedcode/mergeArray.java
@@ -8,6 +8,7 @@ public class mergeArray {
     public static void main(String[] args) {
         int[] num1 = new int[]{1, 2, 3, 0, 0, 0};
         int[] num2 = new int[]{2, 5, 6};
+        merge(num1,3,num2,3);
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -22,6 +23,13 @@ public class mergeArray {
             // compare two elements from nums1 and nums2
             // and add the largest one in nums1
             nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        /**
+         *             if(nums1[p1] < nums2[p2])
+         *                 nums1[p--] = nums2[p2--];
+         *             else
+         *                 nums1[p--] = nums1[p1--];
+         */
 
         // add missing elements from nums2
         System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
