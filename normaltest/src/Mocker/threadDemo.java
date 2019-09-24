65a015c86cb0a4eed5ca74489c67f47ad12ad375
feiyu <feiyu.ji@cognizant.com>
2019-09-24 17:12:14 +0800

    update

--- normaltest/src/Mocker/threadDemo.java
+++ normaltest/src/Mocker/threadDemo.java
@@ -1,9 +1,11 @@
 package Mocker;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class threadDemo {
     static AtomicInteger cnt = new AtomicInteger(0);
+    static ConcurrentHashMap<String, String> hashMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
 
@@ -16,8 +18,10 @@ public class threadDemo {
 //            }
 //        });
 //        thread1.start();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             threadMultle(i);
+            threadOne();
+            threadTwo();
         }
         System.out.println(cnt + "********************************************************************************************************");
     }
@@ -30,8 +34,9 @@ public class threadDemo {
             @Override
             public void run() {
                 try {
-                    if (i >= 999) {
-                        threadException();
+                    if (i >= 95) {
+                        String message = threadException();
+                        hashMap.put("exception", message);
                     } else System.out.println(Thread.currentThread().getName() + ":" + i);
                     Thread.sleep(300);
                 } catch (Exception e) {
@@ -42,11 +47,34 @@ public class threadDemo {
         cnt.incrementAndGet();
     }
 
-    public static void threadException() {
+    public static void threadOne() {
+        String combine = "this is";
+        for (int i = 0; i < 15; i++) {
+            String plan = ":" + i;
+            combine += plan;
+        }
+        String finalCombine = combine;
+        new Thread(() -> System.out.println(hashMap.put("no", finalCombine) + Thread.currentThread().getName())).start();
+        cnt.incrementAndGet();
+    }
+
+    public static void threadTwo() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new Thread(() -> System.out.println("inner thread" + Thread.currentThread().getName())).start();
+            }
+        }).start();
+        cnt.incrementAndGet();
+    }
+
+    public static String threadException() {
+        String message = "";
         try {
-            throw new Exception("测试的异常");
+            throw new Exception(message = "测试的异常");
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return message;
     }
 }
